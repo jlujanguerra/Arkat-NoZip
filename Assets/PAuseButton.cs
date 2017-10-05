5cc00c8111c4cfe44ff5e94f71b8dc6ac98dd772
Daniela Usme <daniela.-.usme@hotmail.com>
2017-10-04 20:02:05 -0500

    FirstCommit

---
+++ Assets/PAuseButton.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PAuseButton : MonoBehaviour {
+    bool hayPausa;
+    public Canvas menuPausa;
+    public GameObject tienda;
+    public Text textLanitas;
+    int lanitas;
+    public SpriteRenderer spriteJugador;
+    public Sprite algo;
+
+    public void BotonTienda()
+    {
+            tienda.gameObject.SetActive(true);
+    }
+
+    public void BotonSalirTienda()
+    {
+        tienda.gameObject.SetActive(false);
+    }
+
+    public void CambiarSkin()
+    {
+        if (lanitas>=100)
+        {
+            lanitas = lanitas-100;
+            spriteJugador.sprite = algo;
+        }
+        else
+        {
+            Debug.Log("Oye no te alcanza! att: Juli =)");
+        }
+    }
+
+    public void BotonPausa()
+    {
+        if (!hayPausa)
+        {
+            Time.timeScale = 0;
+            hayPausa = true;
+            menuPausa.gameObject.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            hayPausa = false;
+            menuPausa.gameObject.SetActive(false);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        hayPausa = false;
+        lanitas = 200;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        textLanitas.text = lanitas.ToString();
+	}
+}
---
+++ Assets/Scripts/JugadorBarra.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JugadorBarra : MonoBehaviour {
+
+    public float VelocidadJugadorBarra;
+    public Vector3 PosicionJugadorBarra; 
+
+	//Use this for initialization
+	void Start () {
+
+       PosicionJugadorBarra = gameObject.transform.position;
+        VelocidadJugadorBarra = 200;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+        if (Input.GetKey(KeyCode.RightArrow)|| Input.GetKey(KeyCode.D))
+        {
+            
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(VelocidadJugadorBarra,0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-VelocidadJugadorBarra, 0);
+        }
+        if (!Input.GetKey(KeyCode.RightArrow)&& !Input.GetKey(KeyCode.LeftArrow)&&!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+	}
+}
